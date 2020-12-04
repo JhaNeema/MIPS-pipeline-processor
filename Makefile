d0f66d85fd304934d808d2ccb35058f476aad95e
JhaNeema <neema.jha17@gmail.com>
2020-12-04 00:54:08 -0800

    Add SVAs for EXE stage. General cleanup.

--- Makefile
+++ Makefile
@@ -1,18 +1,20 @@
+help:
+	@echo "targets: clean compile formal debug"
+	@echo -e "\t clean = cleans project files"
+	@echo -e "\t compile = cleans project files, compiles model & FPV"
+	@echo -e "\t formal = cleans project files, compiles model & FPV, displays FPV results in command line"
+	@echo -e "\t debug = cleans project files, compiles model & FPV, displays FPV results in command line, and runs debug session in GUI"
 
-run: clean compile formal
-mips: clean compile formal debug
-exe: clean compile formal_exe debug
-hdu: clean compile formal_hdu debug
-cu: clean compile formal_cu debug
-
-
-compile:
+compile: clean
+	@echo "Creating project..."
 	vlib work
 	vmap work work
+	@echo "Compiling model & FPV files..."
 	vlog ./modules/*.v ./modules/*/*.v ./defines.v ./topLevelCircuit.v
 	vlog -sv -mfcu -cuname my_bind_sva ./sva_bind.sv ./assertions.sv
 
-formal:
+formal: clean compile
+	@echo "Starting FPV..."
 	qverify -c -od Output_Results -do "\
 		do qs_files/directives.tcl; \
 		formal compile -d MIPS_Processor -cuname my_bind_sva \
@@ -20,38 +22,13 @@ formal:
 		formal verify -init qs_files/myinit.init \
 		-timeout 5m -auto_constraint_off; \
 		exit"
-	
-formal_exe:
-	qverify -c -od Output_Results -do "\
-		do qs_files/directives.tcl; \
-		formal compile -d EXEStage -cuname my_bind_sva \
-			-target_cover_statements; \
-		formal verify -init qs_files/myinit.init \
-		-timeout 5m -auto_constraint_off; \
-		exit"
-		
-formal_hdu:
-	qverify -c -od Output_Results -do "\
-		do qs_files/directives.tcl; \
-		formal compile -d hazard_detection -cuname my_bind_sva \
-			-target_cover_statements; \
-		formal verify \
-		-timeout 5m -auto_constraint_off; \
-		exit"
 
-formal_cu:
-	qverify -c -od Output_Results -do "\
-		do qs_files/directives.tcl; \
-		formal compile -d controller_non_combo -cuname my_bind_sva \
-			-target_cover_statements; \
-		formal verify -init qs_files/myinit.init \
-		-timeout 5m -auto_constraint_off; \
-		exit"
-
-debug: 
+debug: clean compile formal
+	@echo "Compiling FPV debug station..."
 	qverify Output_Results/formal_verify.db &
 
 clean:
+	@echo "Cleaning files..."
 	qverify_clean
 	\rm -rf work modelsim.ini *.wlf *.log replay* transcript *.db
 	\rm -rf Output_Results *.tcl 
--- assertions.sv
+++ assertions.sv
@@ -1,64 +1,58 @@
 `include "defines.v"
 
-`define FPV_ARITH_OPS {`OP_ADD, `OP_SUB, `OP_SLA, `OP_SRA}
-`define FPV_ARITHIMM_OPS {`OP_ADDI, `OP_SUBI}
-`define FPV_LOG_OPS {`OP_AND, `OP_OR, `OP_NOR, `OP_XOR, `OP_SLL, `OP_SRL}
-`define FPV_MEM_OPS {`OP_LD, `OP_ST}
-`define FPV_BR_OPS {`OP_BEZ, `OP_BNE, `OP_JMP}
-`define FPV_OTHER_OPS {`OP_NOP}
-
 `define FPV_TEST_OPS {`OP_ADD, `OP_SUB, `OP_SLA, `OP_SRA, `OP_ADDI, `OP_SUBI, `OP_AND, `OP_OR, `OP_NOR, `OP_XOR, `OP_SLL, `OP_SRL, `OP_LD, `OP_ST}
-
 `define FPV_EXE_CMD {`EXE_ADD, `EXE_SUB, `EXE_AND, `EXE_OR, `EXE_NOR, `EXE_XOR, `EXE_SLA, `EXE_SLL, `EXE_SRA, `EXE_SRL, `EXE_NO_OPERATION}
 
-
-module fpv_stages(clk, rst, forward_EN, hazard_detected, inst_ID, reg1_ID, reg2_ID, EXE_CMD_EXE, val1_sel, val2_sel, ST_val_sel, val1_EXE, val2_EXE, WB_result, ST_value_EXE, PC_EXE, dest_EXE, MEM_R_EN_EXE, MEM_W_EN_EXE, WB_EN_EXE, PC_MEM, dataMem_out_MEM, dest_MEM, WB_EN_MEM, WB_EN_WB, MEM_R_EN_WB, ALURes_WB, dataMem_out_WB, dest_WB, ALURes_MEM, ALURes_EXE, ST_value_EXE2MEM, ST_value_MEM, MEM_R_EN_MEM, MEM_W_EN_MEM, val1_ID, val2_ID, src1_ID, src2_regFile_ID, src2_forw_ID, EXE_CMD_ID, Br_Taken_ID, MEM_R_EN_ID, MEM_W_EN_ID, WB_EN_ID, branch_comm, PC_ID, src2_forw_EXE, src1_forw_EXE, Br_Taken_EXE, is_imm, ST_or_BNE, PC_IF, inst_IF, IF_Flush);
-
-	input clk;
-	input rst;
-	input forward_EN;
-	
-	input [`WORD_LEN-1:0] PC_IF, PC_ID, PC_EXE, PC_MEM;
-	input [`WORD_LEN-1:0] inst_IF, inst_ID;
-	input [`WORD_LEN-1:0] reg1_ID, reg2_ID, ST_value_EXE, ST_value_EXE2MEM, ST_value_MEM;
-	input [`WORD_LEN-1:0] val1_ID, val1_EXE;
-	input [`WORD_LEN-1:0] val2_ID, val2_EXE;
-	input [`WORD_LEN-1:0] ALURes_EXE, ALURes_MEM, ALURes_WB;
-	input [`WORD_LEN-1:0] dataMem_out_MEM, dataMem_out_WB;
-	input [`WORD_LEN-1:0] WB_result;
-	input [`REG_FILE_ADDR_LEN-1:0] dest_EXE, dest_MEM, dest_WB; // dest_ID = instruction[25:21] thus nothing declared
-	input [`REG_FILE_ADDR_LEN-1:0] src1_ID, src2_regFile_ID, src2_forw_ID, src2_forw_EXE, src1_forw_EXE;
-	input [`EXE_CMD_LEN-1:0] EXE_CMD_ID, EXE_CMD_EXE;
-	input [`FORW_SEL_LEN-1:0] val1_sel, val2_sel, ST_val_sel;
-	input [1:0] branch_comm;
-	input Br_Taken_ID, IF_Flush, Br_Taken_EXE;
-	input MEM_R_EN_ID, MEM_R_EN_EXE, MEM_R_EN_MEM, MEM_R_EN_WB;
-	input MEM_W_EN_ID, MEM_W_EN_EXE, MEM_W_EN_MEM;
-	input WB_EN_ID, WB_EN_EXE, WB_EN_MEM, WB_EN_WB;
-	input hazard_detected, is_imm, ST_or_BNE;
-	
+module fpv_stages(
+	input clk,
+	input rst,
+	input forward_EN,
+	input [`WORD_LEN-1:0] PC_IF, PC_ID, PC_EXE, PC_MEM,
+	input [`WORD_LEN-1:0] inst_IF, inst_ID,
+	input [`WORD_LEN-1:0] reg1_ID, reg2_ID, ST_value_EXE, ST_value_EXE2MEM, ST_value_MEM,
+	input [`WORD_LEN-1:0] val1_ID, val1_EXE,
+	input [`WORD_LEN-1:0] val2_ID, val2_EXE,
+	input [`WORD_LEN-1:0] ALURes_EXE, ALURes_MEM, ALURes_WB,
+	input [`WORD_LEN-1:0] dataMem_out_MEM, dataMem_out_WB,
+	input [`WORD_LEN-1:0] WB_result,
+	input [`REG_FILE_ADDR_LEN-1:0] dest_EXE, dest_MEM, dest_WB, // dest_ID = instruction[25:21] thus nothing declared
+	input [`REG_FILE_ADDR_LEN-1:0] src1_ID, src2_regFile_ID, src2_forw_ID, src2_forw_EXE, src1_forw_EXE,
+	input [`EXE_CMD_LEN-1:0] EXE_CMD_ID, EXE_CMD_EXE,
+	input [`FORW_SEL_LEN-1:0] val1_sel, val2_sel, ST_val_sel,
+	input [1:0] branch_comm,
+	input Br_Taken_ID, IF_Flush, Br_Taken_EXE,
+	input MEM_R_EN_ID, MEM_R_EN_EXE, MEM_R_EN_MEM, MEM_R_EN_WB,
+	input MEM_W_EN_ID, MEM_W_EN_EXE, MEM_W_EN_MEM,
+	input WB_EN_ID, WB_EN_EXE, WB_EN_MEM, WB_EN_WB,
+	input hazard_detected, is_imm, ST_or_BNE
+);
+	// Get the opcode from the instruction
 	logic [`OP_CODE_LEN-1:0] opcode = inst_ID[31:26];
 	
 	default clocking c0 @(posedge clk); endclocking;
 	default disable iff (rst);
 	
+	// Properties for operations being processed by the MIPS pipeline from ID to WB stages
+	
+	// Property for arithmetic, immdeiate and logical operation processing by the MIPS pipeline from ID to WB stage
 	property op_arith_logical(op_code, exe_code, opres);
 		logic [`WORD_LEN-1:0] alures; 
 		(inst_ID[31:26] == op_code) |=> ((EXE_CMD_EXE == exe_code) && (val1_sel == 0) && (val2_sel == 0) && !MEM_R_EN_EXE, alures = opres) |-> ALURes_EXE == alures ##1 ALURes_MEM == alures ##1 ((WB_result == alures) && WB_EN_WB );
 	endproperty
 	
+	// Property for load operation processing by the MIPS pipeline from ID to WB stage
 	property op_mem_ld;
 		logic [`WORD_LEN-1:0] alures; 
 		(inst_ID[31:26] == `OP_LD) |=> ((EXE_CMD_EXE == `EXE_ADD) && (val1_sel == 0) && (val2_sel == 0) && MEM_R_EN_EXE, alures = val1_EXE + val2_EXE) |-> ALURes_EXE == alures ##1 (ALURes_MEM == alures) && MEM_R_EN_MEM ##1 ((WB_result == dataMem_out_WB) && WB_EN_WB ) ;
 	endproperty
 	
+	// Property for store operation processing by the MIPS pipeline from ID to MEM stage
 	property op_mem_st;
 		logic [`WORD_LEN-1:0] alures; 
 		(inst_ID[31:26] == `OP_ST) |=> ((EXE_CMD_EXE == `EXE_ADD) && (val1_sel == 0) && (val2_sel == 0) && MEM_W_EN_EXE, alures = val1_EXE + val2_EXE) |-> ALURes_EXE == alures ##1 (ALURes_MEM == alures) && MEM_W_EN_MEM ;
 	endproperty
 	
-	
-	
+	// --- COVERS --- //
 	
 	// Cover only arithmetic, logical, immediate and memory operations
 	op_and_cover: cover property ( opcode == `OP_AND);
@@ -70,15 +64,18 @@ module fpv_stages(clk, rst, forward_EN, hazard_detected, inst_ID, reg1_ID, reg2_
 	// Cover back to back operations
 	backtoback_cover: cover property ( opcode inside `FPV_TEST_OPS ##1 opcode inside `FPV_TEST_OPS ##1 opcode inside `FPV_TEST_OPS ##1 opcode inside `FPV_TEST_OPS );
 	
+	// --- ASSUMPTIONS --- //
+	
 	// Assume only operations which are being verified are input to the design
 	fpv_ops_assume: assume property ( opcode inside `FPV_TEST_OPS);
 	
-	// Assume that src1 and src2 are not the same
+	// Assume that cutpoints src1 and src2 are not the same
 	src1_src2_notsame_assume: assume property ( src1_ID != src2_regFile_ID );
 	
 	// Assume no forwarding
 	nofwd_assume: assume property ( forward_EN == 0 );
-	//nohaz_assume: assume property ( hazard_detected == 0 );
+	
+	// --- ASSERTIONS --- //
 	
 	// Assert arithmetic operations give appropriate results: ADD, SUB
 	add_assert: assert property ( op_arith_logical(`OP_ADD, `EXE_ADD, (val1_EXE + val2_EXE)) );
@@ -102,7 +99,7 @@ module fpv_stages(clk, rst, forward_EN, hazard_detected, inst_ID, reg1_ID, reg2_
 	ld_assert: assert property ( op_mem_ld );
 	st_assert: assert property ( op_mem_st );
 
-endmodule
+endmodule //fpv_stages
 
 
 module fpv_exestage(
@@ -113,35 +110,78 @@ module fpv_exestage(
 );
 
 	default clocking c0 @(posedge clk); endclocking;
-
-	add_cover: cover property ( EXE_CMD inside `FPV_TEST_OPS );
 	
-	add_assert: assert property ( (EXE_CMD == `EXE_ADD) && (val1_sel == 0) && (val2_sel == 0)  |-> ALUResult == (val1 + val2));
-	and_assert: assert property ( (EXE_CMD == `EXE_AND) && (val1_sel == 0) && (val2_sel == 0)  |-> ALUResult == (val1 & val2));
+	
+	// Properties for operations being processed by the MIPS pipeline from ID to WB stages
+	
+	// Property for arithmetic, immdeiate and logical operation processing by the MIPS pipeline from ID to WB stage
+	property op_arith_logical(exe_code, alures);
+		(EXE_CMD == exe_code) && (val1_sel == 0) && (val2_sel == 0) |-> ALUResult == alures;
+	endproperty
+	
+	// --- COVER --- //
+	
+	// Cover back-to-back operations
+	op_cover: cover property ( EXE_CMD inside `FPV_TEST_OPS ##1 EXE_CMD inside `FPV_TEST_OPS );
+	
+	// --- ASSERTIONS --- //
+	
+	// Assert arithmetic operations give appropriate results: ADD, SUB
+	add_assert: assert property ( op_arith_logical(`EXE_ADD, (val1 + val2)) );
+	sub_assert: assert property ( op_arith_logical(`EXE_SUB, (val1 - val2)) );
+	sla_assert: assert property ( op_arith_logical(`EXE_SLA, (val1 << val2)) );
+	sra_assert: assert property ( op_arith_logical(`EXE_SRA, (val1 >> val2)) );
+	
+	// Assert arithmetic immediate operations give appropriate results: ADDI, SUBI
+	addi_assert: assert property ( op_arith_logical(`EXE_ADD, (val1 + val2)) );
+	subi_assert: assert property ( op_arith_logical(`EXE_SUB, (val1 - val2)) );
+	
+	// Assert logical operations give appropriate results: AND, OR, XOR
+	and_assert: assert property ( op_arith_logical(`EXE_AND, (val1 & val2)) );
+	or_assert: assert property ( op_arith_logical(`EXE_OR, (val1 | val2)) );
+	xor_assert: assert property ( op_arith_logical(`EXE_XOR, (val1 ^ val2)) );
+	nor_assert: assert property ( op_arith_logical(`EXE_NOR, ~(val1 | val2)) );
+	sll_assert: assert property ( op_arith_logical(`EXE_SLL, val1 <<< val2) );
+	srl_assert: assert property ( op_arith_logical(`EXE_SRL, val1 >>> val2) );
+	
+	// Assert memory operations - uses ADD to calculate address
 
-endmodule
+endmodule //fpv_exestage
 
 
-module fpv_hazard(forward_EN, is_imm, ST_or_BNE, src1_ID, src2_ID, dest_EXE, WB_EN_EXE, dest_MEM, WB_EN_MEM, MEM_R_EN_EXE, branch_comm, hazard_detected);
-	input logic [`REG_FILE_ADDR_LEN-1:0] src1_ID, src2_ID;
-	input logic [`REG_FILE_ADDR_LEN-1:0] dest_EXE, dest_MEM;
-	input logic [1:0] branch_comm;
-	input logic forward_EN, WB_EN_EXE, WB_EN_MEM, is_imm, ST_or_BNE, MEM_R_EN_EXE;
-	input logic hazard_detected;
+module fpv_hazard(
+	input logic [`REG_FILE_ADDR_LEN-1:0] src1_ID, src2_ID,
+	input logic [`REG_FILE_ADDR_LEN-1:0] dest_EXE, dest_MEM,
+	input logic [1:0] branch_comm,
+	input logic forward_EN, WB_EN_EXE, WB_EN_MEM, is_imm, ST_or_BNE, MEM_R_EN_EXE,
+	input logic hazard_detected
+);
 	
 	logic clk;
 	
 	default clocking c0 @(posedge clk); endclocking;
 	  
-	// Booleans constructs
+	// Booleans for RAW hazards
+	
+	// RAW hazard when same register used as source in ID and destination in EXE
 	let RAW_src1_IDEXE = (src1_ID == dest_EXE) && WB_EN_EXE;
+	
+	// RAW hazard when same register used as 2nd source in ID (which is valid) and destination in EXE
 	let RAW_src2valid_IDEXE = (src2_ID == dest_EXE) && ((~is_imm) || ST_or_BNE) && WB_EN_EXE;
 	
+	// RAW hazard when same register used as source in ID and destination in MEM
 	let RAW_src1_IDMEM = (src1_ID == dest_MEM) && WB_EN_MEM;
+	
+	// RAW hazard when same register used as 2nd source in ID (which is valid) and destination in MEM
 	let RAW_src2valid_IDMEM = (src2_ID == dest_MEM) && ((~is_imm) || ST_or_BNE) && WB_EN_MEM;
 	
+	// All RAW hazards
 	let RAW = RAW_src1_IDEXE || RAW_src2valid_IDEXE || RAW_src1_IDMEM || RAW_src2valid_IDMEM;
+	
+	// All non RAW hazard combination
 	let noRAW = !RAW_src1_IDEXE && !RAW_src2valid_IDEXE && !RAW_src1_IDMEM && !RAW_src2valid_IDMEM;
+	
+	// --- COVERS --- //
 	
 	// Cover different types of RAW hazard waveforms
 	HDU_noRAW_RAW_src1_IDEXE_noRAW: cover property (noRAW ##1 RAW_src1_IDEXE ##1 noRAW);
@@ -154,12 +194,8 @@ module fpv_hazard(forward_EN, is_imm, ST_or_BNE, src1_ID, src2_ID, dest_EXE, WB_
 	
 	// Cover RAW and noRAW waveforms
 	HDU_noRAW_RAW_noRAW: cover property (noRAW ##1 RAW ##1 noRAW);
-
-	// Assume no forwarding
-	HDU_no_forward: assume property(forward_EN == 0 );
 	
-	// Assume src1 and src2 are different registers
-	HDU_src1_src2_not_same: assume property(src1_ID != src2_ID);
+	// --- ASSERTIONS --- //
 
 	// Assert that src1 in ID is the destination of EXE and will be written back in WB, then RAW hazard & hazard detected
 	HDU_src1_IDEXE_RAW: assert property( RAW_src1_IDEXE |->  hazard_detected );
@@ -178,24 +214,25 @@ module fpv_hazard(forward_EN, is_imm, ST_or_BNE, src1_ID, src2_ID, dest_EXE, WB_
 	// Assert that hazard detected is only triggered by a RAW hazard
 	HDU_hazard_triggered: assert property (hazard_detected |-> RAW);
 	
-endmodule
+endmodule //fpv_hazard
 
 
-module fpv_controller(opCode, branchEn, EXE_CMD, Branch_command, Is_Imm, ST_or_BNE, WB_EN, MEM_R_EN, MEM_W_EN, hazard_detected);
-
-	input logic hazard_detected;
-	input logic [`OP_CODE_LEN-1:0] opCode;
-	input logic branchEn;
-	input logic [`EXE_CMD_LEN-1:0] EXE_CMD;
-	input logic [1:0] Branch_command;
-	input logic Is_Imm, ST_or_BNE, WB_EN, MEM_R_EN, MEM_W_EN;
+module fpv_controller(
+	input logic hazard_detected,
+	input logic [`OP_CODE_LEN-1:0] opCode,
+	input logic branchEn,
+	input logic [`EXE_CMD_LEN-1:0] EXE_CMD,
+	input logic [1:0] Branch_command,
+	input logic Is_Imm, ST_or_BNE, WB_EN, MEM_R_EN, MEM_W_EN
+);
 	
 	logic clk;
 
 	default clocking c0 @(posedge clk); endclocking;
 
-	// Cover that the operations under test when provided to the CU, generates one of the control signals
-
+	// --- COVER --- //
+	
+	// Cover that the operations under verification when provided to the CU, generates one of the control signals
 	CU_cover: cover property ( (opCode inside `FPV_TEST_OPS && hazard_detected == 1'b0) ##1 (EXE_CMD inside `FPV_EXE_CMD) && (Is_Imm || WB_EN || MEM_R_EN || MEM_W_EN) );
 
 	// Cover ADD then ADDI operations to look at the waveforms
@@ -211,15 +248,15 @@ module fpv_controller(opCode, branchEn, EXE_CMD, Branch_command, Is_Imm, ST_or_B
 	// Cover hazard detection can be detected
 	CU_cover_hazard_detected: cover property ( hazard_detected == 1'b1 );
 
-	// Assert arithemtic operations generating correct control signals
+	// --- ASSERTIONS --- //
 
+	// Assert arithemtic operations generating correct control signals
 	CU_add_assert: assert property ((opCode == `OP_ADD && hazard_detected == 1'b0) |-> (EXE_CMD == `EXE_ADD) && !Is_Imm && WB_EN && !MEM_R_EN && !MEM_W_EN);
 	CU_sub_assert: assert property ((opCode == `OP_SUB && hazard_detected == 1'b0) |-> (EXE_CMD == `EXE_SUB) && !Is_Imm && WB_EN && !MEM_R_EN && !MEM_W_EN);
 	CU_sla_assert: assert property ((opCode == `OP_SLA && hazard_detected == 1'b0) |-> (EXE_CMD == `EXE_SLA) && !Is_Imm && WB_EN && !MEM_R_EN && !MEM_W_EN);
 	CU_sra_assert: assert property ((opCode == `OP_SRA && hazard_detected == 1'b0) |-> (EXE_CMD == `EXE_SRA) && !Is_Imm && WB_EN && !MEM_R_EN && !MEM_W_EN);
 
 	// Assert logical operations generating correct control signals
-
 	CU_and_assert: assert property ((opCode == `OP_AND && hazard_detected == 1'b0) |-> (EXE_CMD == `EXE_AND) && !Is_Imm && WB_EN && !MEM_R_EN && !MEM_W_EN);
 	CU_or_assert: assert property ((opCode == `OP_OR && hazard_detected == 1'b0) |-> (EXE_CMD == `EXE_OR) && !Is_Imm && WB_EN && !MEM_R_EN && !MEM_W_EN);
 	CU_nor_assert: assert property ((opCode == `OP_NOR && hazard_detected == 1'b0) |-> (EXE_CMD == `EXE_NOR) && !Is_Imm && WB_EN && !MEM_R_EN && !MEM_W_EN);
@@ -228,17 +265,14 @@ module fpv_controller(opCode, branchEn, EXE_CMD, Branch_command, Is_Imm, ST_or_B
 	CU_srl_assert: assert property ((opCode == `OP_SRL && hazard_detected == 1'b0) |-> (EXE_CMD == `EXE_SRL) && !Is_Imm && WB_EN && !MEM_R_EN && !MEM_W_EN);
 
 	// Assert arithmetic immediate operations generating correct control signals
-
 	CU_addi_assert: assert property ((opCode == `OP_ADDI && hazard_detected == 1'b0) |-> (EXE_CMD == `EXE_ADD) && Is_Imm && WB_EN && !MEM_R_EN && !MEM_W_EN);
 	CU_subi_assert: assert property ((opCode == `OP_SUBI && hazard_detected == 1'b0) |-> (EXE_CMD == `EXE_SUB) && Is_Imm && WB_EN && !MEM_R_EN && !MEM_W_EN);
 
 	// Assert memory operations generating correct control signals
-
 	CU_ld_assert: assert property ((opCode == `OP_LD && hazard_detected == 1'b0) |-> (EXE_CMD == `EXE_ADD) && Is_Imm && WB_EN && MEM_R_EN && !MEM_W_EN);
 	CU_st_assert: assert property ((opCode == `OP_ST && hazard_detected == 1'b0) |-> (EXE_CMD == `EXE_ADD) && Is_Imm && !WB_EN && !MEM_R_EN && MEM_W_EN);
 
 	// Assert that no writing can be done when hazard is detected
-
 	CU_hazard_detected: assert property ( ##1 (hazard_detected == 1'b1) |-> (EXE_CMD == `EXE_NO_OPERATION) && !WB_EN && !MEM_W_EN);
 
-endmodule
+endmodule //fpv_controller
