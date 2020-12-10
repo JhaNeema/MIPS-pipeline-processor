806d33fcc3e58f50eaa6f017ee71c3d593396580
JhaNeema <neema.jha17@gmail.com>
2020-12-09 23:16:57 -0800

    Link module instance inside MIPS pipeline to verification.

--- sva_bind.sv
+++ sva_bind.sv
@@ -1,4 +1,4 @@
-bind controller_non_combo fpv_controller fpv_model_cont(.*);
-bind hazard_detection fpv_hazard fpv_model_hdu(.*);
-bind EXEStage fpv_exestage fpv_model_exe(.*);
+bind MIPS_Processor.IDStage.controller fpv_controller fpv_model_cont(.*);
+bind MIPS_Processor.hazard fpv_hazard fpv_model_hdu(.*);
+bind MIPS_Processor.EXEStage fpv_exestage fpv_model_exe(.*);
 bind MIPS_Processor fpv_stages fpv_model_mips(.*);
